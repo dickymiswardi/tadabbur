ea673f9d5131bff9b0bd906d7f6513a9fcab8aea
Dicky Miswardi <dicky_miswardi@yahoo.com>
2025-06-19 10:29:54 +0700

    Create get-audio.js

---
+++ netlify/functions/get-audio.js
@@ -0,0 +1,31 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event) {
+  const { recitation_id, chapter_number } = JSON.parse(event.body);
+
+  const tokenRes = await fetch("https://prelive-oauth2.quran.foundation/v4/auth/oauth/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      grant_type: "client_credentials",
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET
+    })
+  });
+
+  const tokenData = await tokenRes.json();
+
+  const audioRes = await fetch(
+    `https://prelive-oauth2.quran.foundation/v4/audio/recitation/${recitation_id}/audio_files?chapter_number=${chapter_number}`,
+    {
+      headers: { Authorization: `Bearer ${tokenData.access_token}` }
+    }
+  );
+
+  const audioData = await audioRes.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(audioData)
+  };
+};
