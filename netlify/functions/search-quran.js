1e26c465ce2974002d1cc9e9710099b593fb0f19
Dicky Miswardi <dicky_miswardi@yahoo.com>
2025-06-19 11:22:52 +0700

    Update search-quran.js

--- netlify/functions/search-quran.js
+++ netlify/functions/search-quran.js
@@ -3,8 +3,7 @@ const fetch = require("node-fetch");
 exports.handler = async function (event) {
   const { query } = JSON.parse(event.body);
 
-  const res = await fetch(`https://api.quran.com/v4/search?q=${encodeURIComponent(query)}&language=id&size=50`);
-  const data = await res.json();
+  
 
   return {
     statusCode: 200,
