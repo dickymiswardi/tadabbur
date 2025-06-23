0a589d4be2a959202c8ddf15e9ef9da1b4a353a7
Dicky Miswardi <dicky_miswardi@yahoo.com>
2025-06-23 21:58:48 +0700

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,47 +1,51 @@
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('tadabbur-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './quran.json',
-        './indonesian_complex_v1.0.xml',
-        './quran.xml',
-        './TerjemahID.xml',
-        './id.jalalayn.xml',
-        './manifest.json'
-        // Tambah font/css/gambar jika ada
-      ]);
-    })
-  );
-});
-
 self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate') {
     event.respondWith(
       caches.match('./index.html').then(cachedResponse => {
-        return cachedResponse || fetch('./index.html');
+        const fetchPromise = fetch('./index.html').then(networkResponse => {
+          caches.open('tadabbur-cache').then(cache => {
+            cache.put('./index.html', networkResponse.clone());
+          });
+          return networkResponse;
+        }).catch(() => cachedResponse);
+        return cachedResponse || fetchPromise;
       })
     );
     return;
   }
 
+  // Untuk file data: quran.json, xml, dll
+  const dataFiles = [
+    './quran.json',
+    './indonesian_complex_v1.0.xml',
+    './quran.xml',
+    './TerjemahID.xml',
+    './id.jalalayn.xml'
+  ];
+
+  if (dataFiles.includes(new URL(event.request.url).pathname.replace(location.pathname.replace(/\/$/, ''), '.'))) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          caches.open('tadabbur-cache').then(cache => {
+            cache.put(event.request, networkResponse.clone());
+          });
+          return networkResponse;
+        }).catch(() => cachedResponse);
+        return cachedResponse || fetchPromise;
+      })
+    );
+    return;
+  }
+
+  // Default: cache first
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request).then(networkResponse => {
-        return caches.open('tadabbur-cache').then(cache => {
+      return cachedResponse || fetch(event.request).then(networkResponse => {
+        caches.open('tadabbur-cache').then(cache => {
           cache.put(event.request, networkResponse.clone());
-          return networkResponse;
         });
-      }).catch(() => {
-        return new Response("Konten tidak tersedia offline.", {
-          status: 503,
-          statusText: "Offline"
-        });
+        return networkResponse;
       });
     })
   );
